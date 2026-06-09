a614919c5d13bc200f18da750bdf35668a5a8d33
yugg755i <sutariyayug5@gmail.com>
2026-06-10 00:15:49 +0530

    feat: add argparse CLI interface

--- src/main.py
+++ src/main.py
@@ -1,6 +1,43 @@
-from checks import check_world_writable_files
-from reporter import print_reports
+import argparse
+from pathlib import Path
 
-results = [check_world_writable_files()]
+from checks import (
+    check_firewall,
+    check_pass_auth,
+    check_ssh_root_login,
+    check_sudo_users,
+    check_world_writable_files,
+)
+from reporter import print_reports, save_reports_json, save_reports_txt
 
-print_reports(results)
+
+def main():
+    Path("reports").mkdir(exist_ok=True)
+
+    parser = argparse.ArgumentParser(description="linux security auditing tool")
+    parser.add_argument(
+        "--save-txt", action="store_true", help="save report to TXT file"
+    )
+    parser.add_argument(
+        "--save-json", action="store_true", help="save report to JSON file"
+    )
+
+    args = parser.parse_args()
+
+    results = [
+        check_ssh_root_login(),
+        check_pass_auth(),
+        check_firewall(),
+        check_sudo_users(),
+        check_world_writable_files(),
+    ]
+    if args.save_txt:
+        save_reports_txt(results, "reports/report.txt")
+    if args.save_json:
+        save_reports_json(results, "reports/report.json")
+
+    print_reports(results)
+
+
+if __name__ == "__main__":
+    main()
