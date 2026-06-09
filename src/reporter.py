8763e6f6b663670f129bbd53851619b84e311dab
yugg755i <sutariyayug5@gmail.com>
2026-06-10 00:15:02 +0530

    feat: add save options

--- src/reporter.py
+++ src/reporter.py
@@ -1,3 +1,17 @@
+import json
+
+
 def print_reports(results):
     for result in results:
         print(f"[{result['severity']}] {result['message']}")
+
+
+def save_reports_txt(results, filename):
+    with open(filename, "w") as f:
+        for result in results:
+            f.write(f"[{result['severity']}] {result['message']}\n")
+
+
+def save_reports_json(results, filename):
+    with open(filename, "w") as f:
+        json.dump(results, f, indent=4)
